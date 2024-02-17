de364d9424bfc06b4e387155f8c6347fddbb11a6
Anyamba <achuku@gmail.com>
2024-02-17 10:38:30 +0300

    Reconstructed the logic into the function called getComputerChoice()

--- rps.js
+++ rps.js
@@ -1,9 +1,15 @@
 
+// Generate a random choice for the computer
+    function getCompuerChoice() {
         const choices = ['rock','paper','scissors'];
         const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+        return computerChoice;
+
+    }
+    console.log(getCompuerChoice());    
         
         
-        console.log(computerChoice);
+   
 
 
    
