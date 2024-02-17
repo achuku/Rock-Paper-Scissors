a18ab072244f24dfb77a0316e0123145e5adf9ec
Anyamba <achuku@gmail.com>
2024-02-17 10:28:35 +0300

    Logic to randomly return either rock, paper or scissors

---
+++ rps.js
@@ -0,0 +1,9 @@
+
+        const choices = ['rock','paper','scissors'];
+        const computerChoice = choices[Math.floor(Math.random() * choices.length)];
+        
+        
+        console.log(computerChoice);
+
+
+   
