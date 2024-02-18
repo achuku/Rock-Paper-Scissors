3586ddb16d13c4a5e42c30144a1f435e39652743
Anyamba <achuku@gmail.com>
2024-02-18 09:59:49 +0300

    Created function playRound that plays one round of Rock Paper Scissors and returns a verdict - win, loss or tie

--- rps.js
+++ rps.js
@@ -1,14 +1,27 @@
 
-// Generate a random choice for the computer
+// Generate a random choice for the computer's player
     function getCompuerChoice() {
         const choices = ['rock','paper','scissors'];
-        const computerChoice = choices[Math.floor(Math.random() * choices.length)];
-        return computerChoice;
+        const computerSelection = choices[Math.floor(Math.random() * choices.length)];
+        return computerSelection;
 
     }
     console.log(getCompuerChoice());    
         
-        
+ // Play a single round of Rock Paper Scissors
+    function playRound(playerSelection, computerSelection){
+       if (playerSelection === computerSelection){
+        return 'It\'s a tie!'; 
+       }else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
+                 playerSelection === 'paper' && computerSelection === 'rock' ||
+                 playerSelection === 'scissors' && computerSelection === 'paper'  ){
+        return 'You Win!'
+       }else{
+        return 'You Lose.';
+       }
+          
+       
+    }       
    
 
 
