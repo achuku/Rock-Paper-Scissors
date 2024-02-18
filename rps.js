359b6301a4abe7c6d1b3d44b133f4c26507e540e
Anyamba <achuku@gmail.com>
2024-02-18 10:27:16 +0300

    Amended program logic for Rock Paper Scissors to work correctly

--- rps.js
+++ rps.js
@@ -6,22 +6,34 @@
         return computerSelection;
 
     }
-    console.log(getCompuerChoice());    
+       
         
  // Play a single round of Rock Paper Scissors
+    
+    const playerSelection = prompt('Make your choice: rock, paper or scissors', '') ;
+    const computerSelection = getCompuerChoice();
+    console.log(computerSelection); 
+
     function playRound(playerSelection, computerSelection){
-       if (playerSelection === computerSelection){
+       if ( playerSelection === computerSelection ){
         return 'It\'s a tie!'; 
-       }else if (playerSelection === 'rock' && computerSelection === 'scissors' ||
-                 playerSelection === 'paper' && computerSelection === 'rock' ||
-                 playerSelection === 'scissors' && computerSelection === 'paper'  ){
-        return 'You Win!'
-       }else{
-        return 'You Lose.';
+        }else if (  playerSelection === 'rock' && computerSelection === 'scissors' ||
+                    playerSelection === 'paper' && computerSelection === 'rock' ||
+                    playerSelection === 'scissors' && computerSelection === 'paper'  ){
+        return 'You Win!';
+       }else if (   playerSelection === 'rock' && computerSelection === 'paper' ||
+                    playerSelection === 'paper' && computerSelection === 'scissors' ||
+                    playerSelection === 'scissors' && computerSelection === 'rock' ) {
+        return 'Computer Wins!';
+       }else {
+        return 'You Lose!';
        }
-          
-       
-    }       
+    }   
+    
+    
+    console.log(playRound(playerSelection, computerSelection));
+    
+    
    
 
 
