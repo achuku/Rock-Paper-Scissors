df2dd368e59b67fa516f29f02e1d173927f55cd7
Anyamba <achuku@gmail.com>
2024-02-18 10:46:36 +0300

    Played a 5 round game through playGame function, reporting the winner or loser in each round

--- rps.js
+++ rps.js
@@ -7,29 +7,37 @@
 
     }
        
-        
- // Play a single round of Rock Paper Scissors
-    
-    const playerSelection = prompt('Make your choice: rock, paper or scissors', '') ;
-    const computerSelection = getCompuerChoice();
-    console.log(computerSelection); 
+    function playGame(){
+        // Play a single round of Rock Paper Scissors
+            
+        const playerSelection = prompt('Make your choice: rock, paper or scissors', '') ;
+        const computerSelection = getCompuerChoice();
+        console.log(computerSelection); 
 
-    function playRound(playerSelection, computerSelection){
-       if ( playerSelection === computerSelection ){
-        return 'It\'s a tie!'; 
-        }else if (  playerSelection === 'rock' && computerSelection === 'scissors' ||
-                    playerSelection === 'paper' && computerSelection === 'rock' ||
-                    playerSelection === 'scissors' && computerSelection === 'paper'  ){
-        return 'You Win!';
-       }else if (   playerSelection === 'rock' && computerSelection === 'paper' ||
-                    playerSelection === 'paper' && computerSelection === 'scissors' ||
-                    playerSelection === 'scissors' && computerSelection === 'rock' ) {
-        return 'Computer Wins, You Lose!';
-       }
+        function playRound(playerSelection, computerSelection){
+        if ( playerSelection === computerSelection ){
+            return 'It\'s a tie!'; 
+            }else if (  playerSelection === 'rock' && computerSelection === 'scissors' ||
+                        playerSelection === 'paper' && computerSelection === 'rock' ||
+                        playerSelection === 'scissors' && computerSelection === 'paper'  ){
+            return 'You Win!';
+        }else if (   playerSelection === 'rock' && computerSelection === 'paper' ||
+                        playerSelection === 'paper' && computerSelection === 'scissors' ||
+                        playerSelection === 'scissors' && computerSelection === 'rock' ) {
+            return 'Computer Wins, You Lose!';
+        }
+        }   
+        
+        console.log(playRound(playerSelection, computerSelection));
     }   
     
-    
-    console.log(playRound(playerSelection, computerSelection));
+    //Play 5 round game and display winner
+    playGame();
+    playGame();
+    playGame();
+    playGame();
+    playGame();
+        
     
     
    
